e700b18e6abe045a515f578b422b59ba88803e40
Martin Jambon <martin@mjambon.com>
2015-08-10 12:49:28 -0700

    Fix for recent ocamlbuild because an internal function was used and its type changed . Now we're incompatible with previous versions of ocaml for no good reason.

--- src/myocamlbuild.ml
+++ src/myocamlbuild.ml
@@ -8,7 +8,7 @@ let blank_sep_strings = Ocamlbuild_pack.Lexers.blank_sep_strings
 
 (* this lists all supported packages *)
 let find_packages () =
-  blank_sep_strings &
+  blank_sep_strings "" &
     Lexing.from_string &
       run_and_read "ocamlfind list | cut -d' ' -f1"
 
